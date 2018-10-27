d77346c65c50410e651a1a1876c3736a719c3377
Spiderixius <silentwalk15@gmail.com>
2018-10-27 16:44:42 +0200

    Added some more.

---
+++ app/src/main/java/dk/softwareengineering/shoppingoffer/DetailedProductActivity.java
@@ -0,0 +1,13 @@
+package dk.softwareengineering.shoppingoffer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DetailedProductActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detailed_product);
+    }
+}
---
+++ app/src/main/java/dk/softwareengineering/shoppingoffer/MyCouponsActivity.java
@@ -0,0 +1,13 @@
+package dk.softwareengineering.shoppingoffer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MyCouponsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_coupons);
+    }
+}
