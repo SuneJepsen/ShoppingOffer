2d8f63d28d9ba177b64d81e297a38d299f0e1ed5
Spiderixius <silentwalk15@gmail.com>
2018-10-28 06:30:24 +0100

    Added RecycleView for products.

--- app/src/main/java/dk/softwareengineering/shoppingoffer/HomeScreenActivity.java
+++ app/src/main/java/dk/softwareengineering/shoppingoffer/HomeScreenActivity.java
@@ -1,18 +1,35 @@
 package dk.softwareengineering.shoppingoffer;
 
-import android.content.Intent;
+
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Button;
 
+/**
+ * @TODO Comment and comment purpose of class
+ */
 public class HomeScreenActivity extends AppCompatActivity {
+
+    private FragmentManager fragmentManager;
+    private FragmentTransaction fragmentTransaction;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homescreen);
 
+        fragmentManager = getSupportFragmentManager();
+        fragmentTransaction = fragmentManager.beginTransaction();
+
+        addOffersFragment();
+
+    }
+
+    private void addOffersFragment(){
+        OffersFragmentActivity offersFragmentActivity = new OffersFragmentActivity();
+        fragmentTransaction.add(R.id.offersContainer, offersFragmentActivity);
+        fragmentTransaction.commit();
     }
 }
---
+++ app/src/main/java/dk/softwareengineering/shoppingoffer/OffersFragmentActivity.java
@@ -0,0 +1,63 @@
+package dk.softwareengineering.shoppingoffer;
+
+import android.graphics.Color;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+/**
+ * @TODO Comment and comment purpose of class
+ */
+public class OffersFragmentActivity extends Fragment implements OfferAdapter.ItemClickListener {
+
+    private View rootView;
+    private OfferAdapter adapter;
+    private HomeScreenActivity homeScreen;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        rootView = inflater.inflate(R.layout.activity_offers, container, false);
+
+
+        // data to populate the RecyclerView with
+        ArrayList<Integer> viewImages = new ArrayList<>();
+        viewImages.add(Color.BLUE);
+        viewImages.add(Color.YELLOW);
+        viewImages.add(Color.MAGENTA);
+        viewImages.add(Color.RED);
+        viewImages.add(Color.BLACK);
+
+        ArrayList<String> productNames = new ArrayList<>();
+        productNames.add("Jeans");
+        productNames.add("Shirt");
+        productNames.add("Pants");
+        productNames.add("Shirt");
+        productNames.add("Shoes");
+
+        // Setup RecyclerView
+        RecyclerView recyclerView = (RecyclerView) rootView.findViewById(R.id.rv_offers);
+        LinearLayoutManager horizontalLayoutManager = new LinearLayoutManager(getActivity(), LinearLayoutManager.HORIZONTAL, false);
+        recyclerView.setLayoutManager(horizontalLayoutManager);
+        adapter = new OfferAdapter(rootView.getContext(), viewImages, productNames);
+        adapter.setClickListener(this);
+        recyclerView.setAdapter(adapter);
+
+        return rootView;
+    }
+
+    @Override
+    public void onItemClick(View view, int position) {
+        Toast.makeText(rootView.getContext(), "You clicked " + adapter.getItem(position) + " on item position " + position, Toast.LENGTH_SHORT).show();
+    }
+
+}
