3edba1d29df4397d55fc4e877cf8558265351184
anna ølgaard nielsen <annaolgaardnielsen@annas-MacBook-Pro.local>
2018-11-27 22:23:30 +0100

    navigation bar added

--- app/src/main/java/dk/softwareengineering/shoppingoffer/HomeScreenActivity.java
+++ app/src/main/java/dk/softwareengineering/shoppingoffer/HomeScreenActivity.java
@@ -8,6 +8,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.constraint.ConstraintLayout;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
@@ -28,10 +30,15 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
 
 import java.io.IOException;
+import java.util.ResourceBundle;
 
 import Repository.FakeOfferRepository;
 import Repository.FakeStoreFactory;
@@ -46,7 +53,7 @@ import domain.Offer;
  * @TODO Comment and comment purpose of class
  */
 public class HomeScreenActivity extends AppCompatActivity implements OnMapReadyCallback,GoogleApiClient.ConnectionCallbacks,
-        GoogleApiClient.OnConnectionFailedListener, LocationListener {
+        GoogleApiClient.OnConnectionFailedListener, LocationListener, BottomNavigationView.OnNavigationItemSelectedListener {
 
     private static final int UPDATE_INTERVAL = 1000, FASTEST_INTERVAL = 1000 ;
     private GoogleMap mMap;
@@ -84,10 +91,14 @@ public class HomeScreenActivity extends AppCompatActivity implements OnMapReadyC
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homescreen);
+        setTitle(getResources().getString(R.string.title_activity_HomeScreen));
+        BottomNavigationView navView = (BottomNavigationView)findViewById(R.id.navigation_home);
+        navView.getMenu().getItem(0).setChecked(true);
+        navView.setOnNavigationItemSelectedListener(this);
+
         fragmentManager = getSupportFragmentManager();
         fragmentTransaction = fragmentManager.beginTransaction();
         addOffersFragment();
-        setupMenuBar();
 
         mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
@@ -98,24 +109,13 @@ public class HomeScreenActivity extends AppCompatActivity implements OnMapReadyC
         googleApiClient.connect();
     }
 
-    private void setupMenuBar() {
-        // Menu go to MyCoupons
-        Button btn_myCoupons = (Button) findViewById(R.id.btn_myCoupons);
-        btn_myCoupons.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent i = new Intent(HomeScreenActivity.this, MyCouponsActivity.class);
-                startActivity(i);
-            }
-        });
-    }
-
     private void addOffersFragment(){
         OffersFragmentActivity offersFragmentActivity = new OffersFragmentActivity(facade);
         fragmentTransaction.add(R.id.offersContainer, offersFragmentActivity);
+        fragmentTransaction.addToBackStack(null);
         fragmentTransaction.commit();
 
-        
+
     }
 
     /**
@@ -213,5 +213,24 @@ public class HomeScreenActivity extends AppCompatActivity implements OnMapReadyC
         marker = mMap.addMarker(new MarkerOptions().position(current_position).title("Current Position"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(current_position));
         Log.i("GoogleMaps", "current location changed: " + location.getLatitude() + ", " + location.getLongitude());
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        Intent intent = null;
+        switch (item.getItemId()) {
+            case R.id.action_coupons:
+                intent = new Intent(getBaseContext(), MyCouponsActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.action_profile:
+                intent = new Intent(getBaseContext(), ProfileActivity.class);
+                startActivity(intent);
+                break;
+            default:
+                break;
+        }
+
+        return true;
     }
 }
